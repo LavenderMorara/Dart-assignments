f475442c59fc01a260274295c084665ea551f401
chiya morara <chiyamorara@gmail.com>
2025-10-21 01:54:22 +0300

    completed variables

---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main(){
+  int num1 = 5;
+  print(num1);
+
+  double num2 = 8.3;
+  print(num2);
+
+  String word = "Chiya";
+  print(word);
+
+  bool truthy = true;
+  print(truthy);
+
+  List<int> nums = [1, 2, 3, 4];
+  print(nums);
+
+}
